2d4c5a1f5a4131bf791a800a1e2cebdbddcfa615
web-users <srounov@gmail.com>
2015-12-29 00:34:39 +0200

    add files

---
+++ plugin-test/plugin_test.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: Plugin Test
+Plugin URI:  
+Description: Plugin Work Test 
+Author: Sergey
+Version: 1.0
+Author 
+License:
+*/
+
+add_action( 'admin_menu', 'rs_admin_menu' );
+
+
+function rs_admin_menu() {
+	add_menu_page( 'Options plugin (title)', 'Options plugin', 'manage_options','rs-options-plagin', 'rs_option_page','dashicons-admin-generic', '67.4' );
+	add_submenu_page( 'rs-options-plagin', __( 'BackWPup Dashboard', 'backwpup' ), __( 'Dashboard', 'backwpup' ), 'backwpup', 'backwpup', array( 'BackWPup_Page_Backwpup', 'page' ) );
+}
+
+
+function rs_option_page(){
+	$options = get_option( 'rs_plagin_options' );
+
+?>
+
+<div class="wrap">
+	<h2>Options plugin</h2>
+	<p>Settings Plugin &amp; Settings API</p>
+	<p>Add hook new page or posts [test_new_hook]</p>
+		<h2>Application for inventory</h2>
+
+</div>
+
+<?php
+
+
+}
