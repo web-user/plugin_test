16befa084241f1b568b97c3330c8f4a4c0a54bf7
web-users <srounov@gmail.com>
2015-12-29 02:30:27 +0200

    add folder

--- plugin-test/plugin_test.php
+++ plugin-test/plugin_test.php
@@ -13,11 +13,16 @@ add_action( 'admin_menu', 'rs_admin_menu' );
 
 
 function rs_admin_menu() {
-	add_menu_page( 'Options plugin (title)', 'Options plugin', 'manage_options','rs-options-plagin', 'rs_option_page','dashicons-admin-generic', '67.4' );
-	add_submenu_page( 'rs-options-plagin', __( 'BackWPup Dashboard', 'backwpup' ), __( 'Dashboard', 'backwpup' ), 'backwpup', 'backwpup', array( 'BackWPup_Page_Backwpup', 'page' ) );
+	add_menu_page( 'Pure Theme', 'Pure Theme', 'manage_options','rs_options_plagin', 'rs_option_page','dashicons-admin-generic', '67.4' );
+	add_submenu_page( 'rs_options_plagin', __( 'Home Page Slider', 'rs' ), __( 'Home Page Slider', 'rs' ), 'manage_options', 'rs_general_options', 'rs_option_submenu_general' );
+	add_submenu_page( 'rs_options_plagin', __( 'Newew', 'rs' ), __( 'Newew', 'rs' ), 'manage_options', 'rs_general_options_new', 'rs_option_submenu_general_new' );
+	remove_submenu_page('rs_options_plagin','rs_options_plagin');
+
 }
 
 
+
+/*Page*/
 function rs_option_page(){
 	$options = get_option( 'rs_plagin_options' );
 
@@ -32,6 +37,19 @@ function rs_option_page(){
 </div>
 
 <?php
+
+}
+
+/*Page submenu general*/
+function rs_option_submenu_general(){
+	?>
+
+	<div class="wrap">
+		<h2>Options plugin general</h2>
+
+	</div>
+
+	<?php
 
 
 }
