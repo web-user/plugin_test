0000d8fa5bf22590671357abfd6acf12c1b53886
web-users <srounov@gmail.com>
2015-12-29 04:16:07 +0200

    plugin_test

--- plugin-test/plugin_test.php
+++ plugin-test/plugin_test.php
@@ -13,9 +13,9 @@ add_action( 'admin_menu', 'rs_admin_menu' );
 
 
 function rs_admin_menu() {
-	add_menu_page( 'Pure Theme', 'Pure Theme', 'manage_options','rs_options_plagin', 'rs_option_page','dashicons-admin-generic', '67.4' );
-	add_submenu_page( 'rs_options_plagin', __( 'Home Page Slider', 'rs' ), __( 'Home Page Slider', 'rs' ), 'manage_options', 'rs_general_options', 'rs_option_submenu_general' );
-	add_submenu_page( 'rs_options_plagin', __( 'Newew', 'rs' ), __( 'Newew', 'rs' ), 'manage_options', 'rs_general_options_new', 'rs_option_submenu_general_new' );
+	add_menu_page( 'Pure Theme', 'Pure Theme', 'manage_options','rs_options_plagin', 'rs_option_page','dashicons-admin-generic', '8.2' );
+	add_submenu_page( 'rs_options_plagin', __( 'Home Page Slider', 'rs' ), __( 'Home Page Slider', 'rs' ), 'manage_options', 'rs_general_options', 'rs_option_page' );
+	add_submenu_page( 'rs_options_plagin', __( 'Newew', 'rs' ), __( 'Newew', 'rs' ), 'manage_options', 'rs_general_options_new', 'rs_option_submenu_general' );
 	remove_submenu_page('rs_options_plagin','rs_options_plagin');
 
 }
@@ -52,4 +52,20 @@ function rs_option_submenu_general(){
 	<?php
 
 
+
+
 }
+
+function nacin_register_slideshows_post_type() {
+        register_post_type( 'slideshow', array(
+                'labels' => array(
+                        'name' => 'Slideshows',
+                        'singular_name' => 'Slideshow',
+                ),
+                'public' => true,
+                'show_ui' => true,
+                'show_in_menu' => 'rs_options_plagin',
+                'supports' => array( 'title' ,'thumbnail', 'editor' ),
+        ) );
+}
+add_action( 'init', 'nacin_register_slideshows_post_type' );
